b29f31491b42b5a1f5600d95cb3fa6fd9887bc13
Antonina <amarikhina@gmail.com>
2020-03-09 21:39:53 +0100

    #1 Login - Added authentication.service.ts and login.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AddChildComponent} from './add-child/add-child.component';
+import {UpdateParentComponent} from './update-parent/update-parent.component';
+import {UpdateChildComponent} from './update-child/update-child.component';
+import {DeleteParentComponent} from './delete-parent/delete-parent.component';
+import {DeleteChildComponent} from './delete-child/delete-child.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'children/add', component: AddChildComponent},
+  {path: 'parents/update', component: UpdateParentComponent},
+  {path: 'children/update/:id', component: UpdateChildComponent},
+  {path: 'parents/delete', component: DeleteParentComponent},
+  {path: 'children/delete/:id', component: DeleteChildComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AddChildComponent} from './add-child/add-child.component';
+import {UpdateChildComponent} from './update-child/update-child.component';
+import {UpdateParentComponent} from './update-parent/update-parent.component';
+import {DeleteParentComponent} from './delete-parent/delete-parent.component';
+import {DeleteChildComponent} from './delete-child/delete-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddChildComponent,
+    UpdateChildComponent,
+    UpdateParentComponent,
+    DeleteParentComponent,
+    DeleteChildComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
